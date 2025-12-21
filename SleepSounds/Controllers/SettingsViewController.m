800c984ee2be0e66325b2371047c505c8ba70992
gongweiqiang <gongweiqiang@zuoyebang.com>
2025-12-21 22:56:46 +0800

    【feat】：增加取appstore评分的跳转

--- SleepSounds/Controllers/SettingsViewController.m
+++ SleepSounds/Controllers/SettingsViewController.m
@@ -15,14 +15,13 @@
   [super viewDidLoad];
   self.title = @"设置";
 
-    CAGradientLayer *gradient = [CAGradientLayer layer];
-    gradient.frame = self.view.bounds;
-    gradient.colors = @[
-      (id)[UIColor colorWithRed:0.05 green:0.0 blue:0.1 alpha:1.0].CGColor,
-      (id)[UIColor colorWithRed:0.0 green:0.1 blue:0.2 alpha:1.0].CGColor
-    ];
-    [self.view.layer insertSublayer:gradient atIndex:0];
-                                                
+  CAGradientLayer *gradient = [CAGradientLayer layer];
+  gradient.frame = self.view.bounds;
+  gradient.colors = @[
+    (id)[UIColor colorWithRed:0.05 green:0.0 blue:0.1 alpha:1.0].CGColor,
+    (id)[UIColor colorWithRed:0.0 green:0.1 blue:0.2 alpha:1.0].CGColor
+  ];
+  [self.view.layer insertSublayer:gradient atIndex:0];
 
   [self setupData];
   [self setupTableView];
@@ -31,33 +30,15 @@
 - (void)setupData {
   self.settingsItems = @[
     @[
-      @{
-          @"title" : @"淡出时长",
-          @"type" : @"click"
-      },
-      @{
-          @"title" : @"更换背景色",
-          @"type" : @"click"
-      },
-      @{
-          @"title" : @"与其他 App 兼容播放",
-          @"type" : @"switch"
-      }
+      @{@"title" : @"淡出时长", @"type" : @"click"},
+      @{@"title" : @"更换背景色", @"type" : @"click"},
+      @{@"title" : @"与其他 App 兼容播放", @"type" : @"switch"}
     ],
-    
+
     @[
-      @{
-          @"title" : @"意见反馈",
-          @"type" : @"click"
-      },
-      @{
-          @"title" : @"去 AppStore 评分",
-          @"type" : @"click"
-      },
-      @{
-          @"title" : @"隐私协议",
-          @"type" : @"click"
-      }
+      @{@"title" : @"意见反馈", @"type" : @"click"},
+      @{@"title" : @"去 AppStore 评分", @"type" : @"click"},
+      @{@"title" : @"隐私协议", @"type" : @"click"}
     ]
   ];
 }
@@ -184,41 +165,104 @@
 - (void)tableView:(UITableView *)tableView
     didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
   [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    
-    NSDictionary *item = self.settingsItems[indexPath.section][indexPath.row];
-    if ([item[@"title"] isEqualToString:@"意见反馈"]) {
-        [self clickFeedback];
-    }
+
+  NSDictionary *item = self.settingsItems[indexPath.section][indexPath.row];
+  NSString *title = item[@"title"];
+
+  if ([title isEqualToString:@"意见反馈"]) {
+    [self clickFeedback];
+  } else if ([title isEqualToString:@"去 AppStore 评分"]) {
+    [self rateApp];
+  }
 }
 
 #pragma mark - Action
-- (void)clickFeedback{
-    
-    
-    UIAlertController *alert =
-        [UIAlertController alertControllerWithTitle:@"意见反馈"
-                                            message:nil
-                                     preferredStyle:UIAlertControllerStyleActionSheet];
-    [alert addAction:[UIAlertAction
-                         actionWithTitle:@"给1066802504@qq.com发邮件"
-                                   style:UIAlertActionStyleDefault
-                                 handler:^(UIAlertAction *_Nonnull action) {
-        NSURL *url = [NSURL URLWithString:@"mailto:1066802504@qq.com?subject=Feedback"];
-        if ([[UIApplication sharedApplication] canOpenURL:url]) {
-            [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
-        }
-    }]];
-    [alert addAction:[UIAlertAction
-                         actionWithTitle:@"复制1066802504@qq.com"
-                                   style:UIAlertActionStyleDefault
-                                 handler:^(UIAlertAction *_Nonnull action) {
-        [UIPasteboard generalPasteboard].string = @"1066802504@qq.com";
-        [self showToast:@"已复制到剪贴板"];
-    }]];
-    [alert addAction:[UIAlertAction actionWithTitle:@"Close"
-                                              style:UIAlertActionStyleCancel
-                                            handler:nil]];
-    [self presentViewController:alert animated:YES completion:nil];
+- (void)clickFeedback {
+
+  UIAlertController *alert = [UIAlertController
+      alertControllerWithTitle:@"意见反馈"
+                       message:nil
+                preferredStyle:UIAlertControllerStyleActionSheet];
+  [alert
+      addAction:[UIAlertAction
+                    actionWithTitle:@"给1066802504@qq.com发邮件"
+                              style:UIAlertActionStyleDefault
+                            handler:^(UIAlertAction *_Nonnull action) {
+                              NSURL *url =
+                                  [NSURL URLWithString:@"mailto:1066802504@qq."
+                                                       @"com?subject=Feedback"];
+                              if ([[UIApplication sharedApplication]
+                                      canOpenURL:url]) {
+                                [[UIApplication sharedApplication] openURL:url
+                                                                   options:@{}
+                                                         completionHandler:nil];
+                              }
+                            }]];
+  [alert addAction:[UIAlertAction
+                       actionWithTitle:@"复制1066802504@qq.com"
+                                 style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction *_Nonnull action) {
+                                 [UIPasteboard generalPasteboard].string =
+                                     @"1066802504@qq.com";
+                                 [self showToast:@"已复制到剪贴板"];
+                               }]];
+  [alert addAction:[UIAlertAction actionWithTitle:@"Close"
+                                            style:UIAlertActionStyleCancel
+                                          handler:nil]];
+  [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)rateApp {
+  // 替换为你的 App ID
+  NSString *appID = @"1330927814";
+  NSString *urlStr = [NSString
+      stringWithFormat:
+          @"itms-apps://itunes.apple.com/app/id%@?action=write-review", appID];
+  NSURL *url = [NSURL URLWithString:urlStr];
+
+  if ([[UIApplication sharedApplication] canOpenURL:url]) {
+    [[UIApplication sharedApplication] openURL:url
+                                       options:@{}
+                             completionHandler:nil];
+  } else {
+    [self showToast:@"无法打开 App Store"];
+  }
+}
+
+- (void)showToast:(NSString *)message {
+  UILabel *label = [[UILabel alloc] init];
+  label.text = message;
+  label.textColor = [UIColor whiteColor];
+  label.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.8];
+  label.textAlignment = NSTextAlignmentCenter;
+  label.font = [UIFont systemFontOfSize:14 weight:UIFontWeightMedium];
+  label.layer.cornerRadius = 20;
+  label.layer.masksToBounds = YES;
+
+  [self.view addSubview:label];
+  [label mas_makeConstraints:^(MASConstraintMaker *make) {
+    make.centerX.equalTo(self.view);
+    make.bottom.equalTo(self.view.mas_safeAreaLayoutGuideBottom).offset(-100);
+    make.width.mas_greaterThanOrEqualTo(200);
+    make.height.mas_equalTo(40);
+  }];
+
+  label.alpha = 0.0;
+  [UIView animateWithDuration:0.3
+      animations:^{
+        label.alpha = 1.0;
+      }
+      completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.3
+            delay:2.0
+            options:0
+            animations:^{
+              label.alpha = 0.0;
+            }
+            completion:^(BOOL finished) {
+              [label removeFromSuperview];
+            }];
+      }];
 }
 
 @end
